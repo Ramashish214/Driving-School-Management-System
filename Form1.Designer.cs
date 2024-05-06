a5601f20291e480e2c5dcbdbfe33eeee98df478a
dnmanamperi <dnmanamperi@gmail.com>
2024-05-06 19:15:06 +0530

    Initialize the project

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(808, 536);
+            this.MaximumSize = new System.Drawing.Size(826, 583);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion

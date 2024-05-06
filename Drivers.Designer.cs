a62f78da7e3b0772664aba655438a55d8a8b3249
dnmanamperi <dnmanamperi@gmail.com>
2024-05-06 22:59:56 +0530

    add driver form components

--- Drivers.Designer.cs
+++ Drivers.Designer.cs
@@ -28,37 +28,208 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dataGridViewDriver = new System.Windows.Forms.DataGridView();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.driverId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.driverName = new System.Windows.Forms.TextBox();
+            this.licenseNo = new System.Windows.Forms.TextBox();
+            this.vehicleInCharge = new System.Windows.Forms.TextBox();
+            this.licenseType = new System.Windows.Forms.ComboBox();
+            this.bloodType = new System.Windows.Forms.ComboBox();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDriver)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dataGridViewDriver
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(16, 15);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(1035, 319);
-            this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridViewDriver.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDriver.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewDriver.Name = "dataGridViewDriver";
+            this.dataGridViewDriver.RowHeadersWidth = 51;
+            this.dataGridViewDriver.Size = new System.Drawing.Size(776, 259);
+            this.dataGridViewDriver.TabIndex = 0;
+            this.dataGridViewDriver.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Location = new System.Drawing.Point(325, 382);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(75, 23);
+            this.AddBtn.TabIndex = 1;
+            this.AddBtn.Text = "Add";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // driverId
+            // 
+            this.driverId.Location = new System.Drawing.Point(157, 297);
+            this.driverId.Name = "driverId";
+            this.driverId.Size = new System.Drawing.Size(121, 20);
+            this.driverId.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 300);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 332);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 362);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "License No";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 392);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "License Type";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 419);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Blood Type";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(55, 445);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Vehicle InCharge";
+            // 
+            // driverName
+            // 
+            this.driverName.Location = new System.Drawing.Point(157, 329);
+            this.driverName.Name = "driverName";
+            this.driverName.Size = new System.Drawing.Size(121, 20);
+            this.driverName.TabIndex = 9;
+            // 
+            // licenseNo
+            // 
+            this.licenseNo.Location = new System.Drawing.Point(157, 359);
+            this.licenseNo.Name = "licenseNo";
+            this.licenseNo.Size = new System.Drawing.Size(121, 20);
+            this.licenseNo.TabIndex = 10;
+            // 
+            // vehicleInCharge
+            // 
+            this.vehicleInCharge.Location = new System.Drawing.Point(157, 445);
+            this.vehicleInCharge.Name = "vehicleInCharge";
+            this.vehicleInCharge.Size = new System.Drawing.Size(121, 20);
+            this.vehicleInCharge.TabIndex = 11;
+            // 
+            // licenseType
+            // 
+            this.licenseType.FormattingEnabled = true;
+            this.licenseType.Items.AddRange(new object[] {
+            "Light Vehicles",
+            "Heavy Vehicles"});
+            this.licenseType.Location = new System.Drawing.Point(157, 385);
+            this.licenseType.Name = "licenseType";
+            this.licenseType.Size = new System.Drawing.Size(121, 21);
+            this.licenseType.TabIndex = 12;
+            // 
+            // bloodType
+            // 
+            this.bloodType.FormattingEnabled = true;
+            this.bloodType.Items.AddRange(new object[] {
+            "A",
+            "A+",
+            "A-",
+            "O+",
+            "O-",
+            "B",
+            "B+"});
+            this.bloodType.Location = new System.Drawing.Point(157, 411);
+            this.bloodType.Name = "bloodType";
+            this.bloodType.Size = new System.Drawing.Size(121, 21);
+            this.bloodType.TabIndex = 13;
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(437, 382);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.deleteBtn.TabIndex = 14;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
             // 
             // Drivers
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ClientSize = new System.Drawing.Size(804, 475);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.bloodType);
+            this.Controls.Add(this.licenseType);
+            this.Controls.Add(this.vehicleInCharge);
+            this.Controls.Add(this.licenseNo);
+            this.Controls.Add(this.driverName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.driverId);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.dataGridViewDriver);
             this.Name = "Drivers";
             this.Text = "Drivers";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.Drivers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDriver)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridViewDriver;
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.TextBox driverId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox driverName;
+        private System.Windows.Forms.TextBox licenseNo;
+        private System.Windows.Forms.TextBox vehicleInCharge;
+        private System.Windows.Forms.ComboBox licenseType;
+        private System.Windows.Forms.ComboBox bloodType;
+        private System.Windows.Forms.Button deleteBtn;
     }
 }
--- Drivers.cs
+++ Drivers.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace driving_school_management_system
 {
@@ -20,9 +21,87 @@ namespace driving_school_management_system
 
         SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"E:\\BSc.Electrical Engineering\\SEM 04\\CodeDnM\\C#\\driving_school_management_system\\dbSystemDSMS.mdf\";Integrated Security=True");
 
+        void BindData()
+        {
+            SqlCommand cmd = new SqlCommand("select * from Driver", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridViewDriver.DataSource = dt;
+        }
+
+
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void Drivers_Load(object sender, EventArgs e)
+        {
+            BindData();
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+
+            // Check if the primary key already exists
+            SqlCommand checkCmd = new SqlCommand("SELECT COUNT(*) FROM Driver WHERE Id = @PrimaryKeyValue", connection);
+            checkCmd.Parameters.AddWithValue("@PrimaryKeyValue", driverId.Text);
+
+            int existingCount = (int)checkCmd.ExecuteScalar();
+            if (existingCount > 0)
+            {
+                MessageBox.Show("Primary key already exists. Please enter a different value.");
+                connection.Close();
+                return; // Exit the event handler
+            }
+
+            // Proceed with insertion if the primary key does not exist
+            
+
+            try
+            {
+                SqlCommand insertCmd = new SqlCommand("INSERT INTO Driver VALUES (@Value1, @Value2, @Value3, @Value4, @Value5, @Value6)", connection);
+                insertCmd.Parameters.AddWithValue("@Value1", driverId.Text);
+                insertCmd.Parameters.AddWithValue("@Value2", driverName.Text);
+                insertCmd.Parameters.AddWithValue("@Value3", int.Parse(licenseNo.Text));
+                insertCmd.Parameters.AddWithValue("@Value4", licenseType.Text);
+                insertCmd.Parameters.AddWithValue("@Value5", bloodType.Text);
+                insertCmd.Parameters.AddWithValue("@Value6", vehicleInCharge.Text);
+                insertCmd.ExecuteNonQuery();
+                MessageBox.Show("Inserted");
+                BindData();
+            }
+            catch (SqlException ex)
+            {
+                // Handle SQL exceptions
+                MessageBox.Show("An error occurred while inserting data: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            BindData();
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("DELETE  Driver  WHERE Id = @ID", connection);
+                cmd.Parameters.AddWithValue("@ID", driverId.Text);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+                MessageBox.Show("Deleted");
+                BindData();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Enter ID");
+            }
+            
         }
     }
 }

ddcc30fcd6d24023782e6e0f252e02220d681fca
dnmanamperi <dnmanamperi@gmail.com>
2024-05-06 23:45:11 +0530

    add vehicle form components

--- Vehicles.Designer.cs
+++ Vehicles.Designer.cs
@@ -28,12 +28,201 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridViewVehicles = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.vehicleNo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewVehicles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewVehicles
+            // 
+            this.dataGridViewVehicles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewVehicles.Location = new System.Drawing.Point(35, 21);
+            this.dataGridViewVehicles.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridViewVehicles.Name = "dataGridViewVehicles";
+            this.dataGridViewVehicles.RowHeadersWidth = 51;
+            this.dataGridViewVehicles.Size = new System.Drawing.Size(996, 277);
+            this.dataGridViewVehicles.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 346);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Vehicle No";
+            // 
+            // vehicleNo
+            // 
+            this.vehicleNo.Location = new System.Drawing.Point(273, 342);
+            this.vehicleNo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.vehicleNo.Name = "vehicleNo";
+            this.vehicleNo.Size = new System.Drawing.Size(160, 22);
+            this.vehicleNo.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 383);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Vehicle Type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 423);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Last Service Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(103, 459);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Next Service Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(103, 496);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Driver Incharge";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(103, 527);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(142, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "License Renewal Date";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Car",
+            "Van"});
+            this.comboBox1.Location = new System.Drawing.Point(273, 379);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(279, 425);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(279, 459);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker2.TabIndex = 11;
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Location = new System.Drawing.Point(279, 527);
+            this.dateTimePicker3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker3.TabIndex = 12;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(279, 491);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(215, 24);
+            this.comboBox2.TabIndex = 13;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(749, 410);
+            this.addBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(100, 28);
+            this.addBtn.TabIndex = 14;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // Vehicles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1120, 591);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.vehicleNo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewVehicles);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Vehicles";
             this.Text = "Vehicles";
+            this.Load += new System.EventHandler(this.Vehicles_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewVehicles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewVehicles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox vehicleNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button addBtn;
     }
 }
--- Vehicles.cs
+++ Vehicles.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,89 @@ namespace driving_school_management_system
         {
             InitializeComponent();
         }
-    }
+
+        SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"E:\\BSc.Electrical Engineering\\SEM 04\\CodeDnM\\C#\\driving_school_management_system\\dbSystemDSMS.mdf\";Integrated Security=True");
+
+        void BindData()
+        {
+            SqlCommand cmd = new SqlCommand("select * from Vehicle", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridViewVehicles.DataSource = dt;
+        }
+
+        private void LoadDriverIDs()
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Id FROM Driver", connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    comboBox2.Items.Add(reader["Id"].ToString());
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading driver IDs: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void Vehicles_Load(object sender, EventArgs e)
+        {
+            BindData();
+            LoadDriverIDs();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+
+            // Check if the primary key already exists
+            SqlCommand checkCmd = new SqlCommand("SELECT COUNT(*) FROM Vehicle WHERE [Vehicle No] = @PrimaryKeyValue", connection);
+            checkCmd.Parameters.AddWithValue("@PrimaryKeyValue", vehicleNo.Text);
+
+            int existingCount = (int)checkCmd.ExecuteScalar();
+            if (existingCount > 0)
+            {
+                MessageBox.Show("Primary key already exists. Please enter a different value.");
+                connection.Close();
+                return; // Exit the event handler
+            }
+
+            // Proceed with insertion if the primary key does not exist
+
+
+            try
+            {
+                SqlCommand insertCmd = new SqlCommand("INSERT INTO Vehicle VALUES (@Value1, @Value2, @Value3, @Value4, @Value5, @Value6)", connection);
+                insertCmd.Parameters.AddWithValue("@Value1", vehicleNo.Text);
+                insertCmd.Parameters.AddWithValue("@Value2", comboBox1.Text);
+                insertCmd.Parameters.AddWithValue("@Value3", dateTimePicker1.Value);
+                insertCmd.Parameters.AddWithValue("@Value4", dateTimePicker2.Value);
+                insertCmd.Parameters.AddWithValue("@Value5", comboBox2.Text);
+                insertCmd.Parameters.AddWithValue("@Value6", dateTimePicker3.Value);
+                insertCmd.ExecuteNonQuery();
+                MessageBox.Show("Inserted");
+                BindData();
+            }
+            catch (SqlException ex)
+            {
+                // Handle SQL exceptions
+                MessageBox.Show("An error occurred while inserting data: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            BindData();
+        }
+    }   
 }

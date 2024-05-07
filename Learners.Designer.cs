bf6e517a92e96cf688d735e7ac88b72a4376bbef
dnmanamperi <dnmanamperi@gmail.com>
2024-05-07 09:10:19 +0530

    add learner components
    table
    form design
    functions

--- Learners.Designer.cs
+++ Learners.Designer.cs
@@ -28,12 +28,306 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridViewLearners = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.nationalId = new System.Windows.Forms.OpenFileDialog();
+            this.birthCertificate = new System.Windows.Forms.OpenFileDialog();
+            this.medicalReport = new System.Windows.Forms.OpenFileDialog();
+            this.writtenExam = new System.Windows.Forms.OpenFileDialog();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLearners)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewLearners
+            // 
+            this.dataGridViewLearners.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewLearners.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewLearners.Name = "dataGridViewLearners";
+            this.dataGridViewLearners.RowHeadersWidth = 51;
+            this.dataGridViewLearners.RowTemplate.Height = 24;
+            this.dataGridViewLearners.Size = new System.Drawing.Size(1242, 228);
+            this.dataGridViewLearners.TabIndex = 0;
+            this.dataGridViewLearners.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewLearners_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 294);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Id";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(180, 291);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 334);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 374);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "License Type";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 409);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "National ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 441);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Birth Certificate";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(59, 475);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Medical Report";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(59, 506);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 16);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Written Exam";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Light Vehicles",
+            "Heavy Vehicles"});
+            this.comboBox1.Location = new System.Drawing.Point(180, 371);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(180, 406);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(121, 22);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(180, 438);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(121, 22);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(180, 469);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(121, 22);
+            this.textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(180, 503);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(121, 22);
+            this.textBox6.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(324, 441);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(324, 475);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(324, 508);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(533, 438);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 19;
+            this.addBtn.Text = "ADD";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // nationalId
+            // 
+            this.nationalId.FileName = "openFileDialog1";
+            // 
+            // birthCertificate
+            // 
+            this.birthCertificate.FileName = "openFileDialog1";
+            // 
+            // medicalReport
+            // 
+            this.medicalReport.FileName = "openFileDialog1";
+            // 
+            // writtenExam
+            // 
+            this.writtenExam.FileName = "openFileDialog1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(611, 293);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 16);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Address";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(180, 328);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(689, 290);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(121, 22);
+            this.textBox7.TabIndex = 21;
+            // 
+            // Learners
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1266, 603);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewLearners);
+            this.Name = "Learners";
             this.Text = "Learners";
+            this.Load += new System.EventHandler(this.Learners_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLearners)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewLearners;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.OpenFileDialog nationalId;
+        private System.Windows.Forms.OpenFileDialog birthCertificate;
+        private System.Windows.Forms.OpenFileDialog medicalReport;
+        private System.Windows.Forms.OpenFileDialog writtenExam;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox7;
     }
 }
--- Learners.cs
+++ Learners.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,224 @@ namespace driving_school_management_system
         public Learners()
         {
             InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"E:\\BSc.Electrical Engineering\\SEM 04\\CodeDnM\\C#\\driving_school_management_system\\dbSystemDSMS.mdf\";Integrated Security=True");
+
+        void BindData()
+        {
+            SqlCommand cmd = new SqlCommand("select * from Learner", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridViewLearners.DataSource = dt;
+
+            dt.Columns.Remove("National ID");
+            dt.Columns.Remove("Birth Certificate");
+            dt.Columns.Remove("Medical Report");
+        }
+
+        private byte[] GetPDFFileBytes(string id, string columnName)
+        {
+            byte[] fileBytes = null;
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand($"SELECT {columnName} FROM Learner WHERE Id = @Id", connection);
+                cmd.Parameters.AddWithValue("@Id", id);
+                object result = cmd.ExecuteScalar();
+                if (result != DBNull.Value)
+                {
+                    fileBytes = (byte[])result;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error retrieving PDF file: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return fileBytes;
+        }
+
+        private void ViewPDF(string id, string columnName)
+        {
+            byte[] fileBytes = GetPDFFileBytes(id, columnName);
+            if (fileBytes != null)
+            {
+                // Save the PDF file to a temporary location
+                string tempFilePath = Path.GetTempFileName() + ".pdf";
+                File.WriteAllBytes(tempFilePath, fileBytes);
+
+                // Check if the file was created successfully
+                if (File.Exists(tempFilePath))
+                {
+                    // Open the PDF file using the default viewer
+                    try
+                    {
+                        System.Diagnostics.Process.Start(tempFilePath);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error opening PDF file: " + ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Failed to save PDF file to temporary location.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No PDF file available for this ID.");
+            }
+        }
+
+
+        private void InsertPDF(string id, byte[] nationalId, byte[] birthCertificate, byte[] MedicalReport)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Learner (Id, [National ID], [Birth Certificate], [Medical Report]) VALUES (@Id, @FileBytes1, @FileBytes2,@FileBytes3)", connection);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                // Check if fileBytes is null and add DBNull.Value if it is
+                if (nationalId == null)
+                {
+                    cmd.Parameters.Add("@FileBytes1", SqlDbType.VarBinary, -1).Value = DBNull.Value;
+                }
+                else
+                {
+                    cmd.Parameters.Add("@FileBytes1", SqlDbType.VarBinary, -1).Value = nationalId;
+                }
+
+                // Check if fileBytes1 is null and add DBNull.Value if it is
+                if (birthCertificate == null)
+                {
+                    cmd.Parameters.Add("@FileBytes2", SqlDbType.VarBinary, -1).Value = DBNull.Value;
+                }
+                else
+                {
+                    cmd.Parameters.Add("@FileBytes2", SqlDbType.VarBinary, -1).Value = birthCertificate;
+                }
+
+                if (MedicalReport == null)
+                {
+                    cmd.Parameters.Add("@FileBytes3", SqlDbType.VarBinary, -1).Value = DBNull.Value;
+                }
+                else
+                {
+                    cmd.Parameters.Add("@FileBytes3", SqlDbType.VarBinary, -1).Value = MedicalReport;
+                }
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("PDF file inserted successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error inserting PDF file: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+                BindData(); // Refresh DataGridView
+            }
+        }
+
+        private void Learners_Load(object sender, EventArgs e)
+        {
+            BindData();
+            DataGridViewButtonColumn buttonColumn = new DataGridViewButtonColumn();
+            buttonColumn.HeaderText = "National ID";
+            buttonColumn.Name = "pdfFile";
+            buttonColumn.Text = "View";
+            buttonColumn.UseColumnTextForButtonValue = true;
+            dataGridViewLearners.Columns.Add(buttonColumn);
+
+            DataGridViewButtonColumn buttonColumn1 = new DataGridViewButtonColumn();
+            buttonColumn1.HeaderText = "Birth Certificate";
+            buttonColumn1.Name = "pdfFile1";
+            buttonColumn1.Text = "View";
+            buttonColumn1.UseColumnTextForButtonValue = true;
+            dataGridViewLearners.Columns.Add(buttonColumn1);
+
+            DataGridViewButtonColumn buttonColumn2 = new DataGridViewButtonColumn();
+            buttonColumn2.HeaderText = "Medical Report";
+            buttonColumn2.Name = "pdfFile2";
+            buttonColumn2.Text = "View";
+            buttonColumn2.UseColumnTextForButtonValue = true;
+            dataGridViewLearners.Columns.Add(buttonColumn2);
+
+            /*DataGridViewButtonColumn buttonColumn3 = new DataGridViewButtonColumn();
+            buttonColumn3.HeaderText = "Written Exam";
+            buttonColumn3.Name = "pdfFile1";
+            buttonColumn3.Text = "View";
+            buttonColumn3.UseColumnTextForButtonValue = true;
+            dataGridViewLearners.Columns.Add(buttonColumn3);*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (nationalId.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = nationalId.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (birthCertificate.ShowDialog() == DialogResult.OK)
+            {
+                textBox4.Text = birthCertificate.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (medicalReport.ShowDialog() == DialogResult.OK)
+            {
+                textBox5.Text = medicalReport.FileName;
+            }
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            // Check if both ID and PDF file are provided
+            if (!string.IsNullOrEmpty(textBox1.Text)) // && !string.IsNullOrEmpty(textBox2.Text))
+            {
+                // Read PDF file bytes
+                byte[] fileBytes = File.ReadAllBytes(textBox3.Text);
+                byte[] fileBytes1 = File.ReadAllBytes(textBox4.Text);
+                byte[] fileBytes2 = File.ReadAllBytes(textBox5.Text);
+                // Insert PDF file bytes into database
+                InsertPDF(textBox1.Text, fileBytes, fileBytes1, fileBytes2);
+                //InsertPDF(textBox1.Text, fileBytes1);
+            }
+            else
+            {
+                //MessageBox.Show("Please provide both ID and PDF file path.");
+                //byte[] fileBytes = File.ReadAllBytes(null);
+                //byte[] fileBytes1 = File.ReadAllBytes(null);
+                InsertPDF(textBox1.Text, null, null,null);
+            }
+        }
+
+        private void dataGridViewLearners_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if ((e.ColumnIndex == dataGridViewLearners.Columns["pdfFile"].Index || e.ColumnIndex == dataGridViewLearners.Columns["pdfFile1"].Index || e.ColumnIndex == dataGridViewLearners.Columns["pdfFile2"].Index && e.RowIndex  >= 0))
+            {
+                // Get the ID from the clicked row
+                string id = dataGridViewLearners.Rows[e.RowIndex].Cells["Id"].Value.ToString();
+
+                // Determine the column name based on the clicked cell
+                string columnName = dataGridViewLearners.Columns[e.ColumnIndex].Name;
+
+                // View the PDF document
+                ViewPDF(id, columnName);
+            }
         }
     }
 }

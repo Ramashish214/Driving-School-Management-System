95f5e1c15b80db1579782a220c21e8bc45921405
dnmanamperi <dnmanamperi@gmail.com>
2024-05-08 10:43:14 +0530

    add calculate function

--- Payements.Designer.cs
+++ Payements.Designer.cs
@@ -46,12 +46,14 @@
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.textBox8 = new System.Windows.Forms.TextBox();
             this.calculateBtn = new System.Windows.Forms.Button();
+            this.printBtn = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(105, 74);
+            this.label1.Location = new System.Drawing.Point(81, 68);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(20, 16);
             this.label1.TabIndex = 0;
@@ -59,7 +61,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(235, 74);
+            this.textBox1.Location = new System.Drawing.Point(211, 68);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 1;
@@ -67,7 +69,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(105, 123);
+            this.label2.Location = new System.Drawing.Point(81, 117);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 16);
             this.label2.TabIndex = 2;
@@ -75,7 +77,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(235, 120);
+            this.textBox2.Location = new System.Drawing.Point(211, 114);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 3;
@@ -83,7 +85,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(105, 161);
+            this.label3.Location = new System.Drawing.Point(81, 155);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(58, 16);
             this.label3.TabIndex = 4;
@@ -92,7 +94,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(105, 196);
+            this.label4.Location = new System.Drawing.Point(81, 190);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(73, 16);
             this.label4.TabIndex = 5;
@@ -101,7 +103,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(105, 232);
+            this.label5.Location = new System.Drawing.Point(81, 226);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(89, 16);
             this.label5.TabIndex = 6;
@@ -109,38 +111,39 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(235, 158);
+            this.textBox3.Location = new System.Drawing.Point(211, 152);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 22);
             this.textBox3.TabIndex = 7;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(235, 196);
+            this.textBox4.Location = new System.Drawing.Point(211, 190);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 22);
             this.textBox4.TabIndex = 8;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(235, 229);
+            this.textBox5.Location = new System.Drawing.Point(211, 223);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 22);
             this.textBox5.TabIndex = 9;
             // 
             // searchBtn
             // 
-            this.searchBtn.Location = new System.Drawing.Point(407, 73);
+            this.searchBtn.Location = new System.Drawing.Point(383, 67);
             this.searchBtn.Name = "searchBtn";
             this.searchBtn.Size = new System.Drawing.Size(75, 23);
             this.searchBtn.TabIndex = 10;
             this.searchBtn.Text = "Search";
             this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(105, 273);
+            this.label6.Location = new System.Drawing.Point(81, 267);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(86, 16);
             this.label6.TabIndex = 11;
@@ -149,7 +152,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(105, 310);
+            this.label7.Location = new System.Drawing.Point(81, 304);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(59, 16);
             this.label7.TabIndex = 12;
@@ -158,7 +161,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(105, 350);
+            this.label8.Location = new System.Drawing.Point(81, 344);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(38, 16);
             this.label8.TabIndex = 13;
@@ -166,39 +169,55 @@
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(235, 270);
+            this.textBox6.Location = new System.Drawing.Point(211, 264);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(100, 22);
             this.textBox6.TabIndex = 14;
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(235, 307);
+            this.textBox7.Location = new System.Drawing.Point(211, 301);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(100, 22);
             this.textBox7.TabIndex = 15;
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(235, 347);
+            this.textBox8.Location = new System.Drawing.Point(211, 341);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(100, 22);
             this.textBox8.TabIndex = 16;
             // 
             // calculateBtn
             // 
-            this.calculateBtn.Location = new System.Drawing.Point(407, 350);
+            this.calculateBtn.Location = new System.Drawing.Point(383, 344);
             this.calculateBtn.Name = "calculateBtn";
-            this.calculateBtn.Size = new System.Drawing.Size(75, 23);
+            this.calculateBtn.Size = new System.Drawing.Size(91, 23);
             this.calculateBtn.TabIndex = 17;
             this.calculateBtn.Text = "Calculate";
             this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // printBtn
+            // 
+            this.printBtn.Location = new System.Drawing.Point(562, 343);
+            this.printBtn.Name = "printBtn";
+            this.printBtn.Size = new System.Drawing.Size(75, 23);
+            this.printBtn.TabIndex = 18;
+            this.printBtn.Text = "Print";
+            this.printBtn.UseVisualStyleBackColor = true;
+            this.printBtn.Click += new System.EventHandler(this.printBtn_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
             // 
             // Payements
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1131, 562);
+            this.Controls.Add(this.printBtn);
             this.Controls.Add(this.calculateBtn);
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.textBox7);
@@ -244,5 +263,7 @@
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.TextBox textBox8;
         private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Button printBtn;
+        private System.Drawing.Printing.PrintDocument printDocument1;
     }
 }
--- Payements.cs
+++ Payements.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace driving_school_management_system
 {
@@ -15,6 +18,95 @@ namespace driving_school_management_system
         public Payements()
         {
             InitializeComponent();
+            printDocument1.PrintPage += printDocument1_PrintPage;
+            printDocument1.DefaultPageSettings.PaperSize = new PaperSize("A5", 583, 827);
+        }
+
+        SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"E:\\BSc.Electrical Engineering\\SEM 04\\CodeDnM\\C#\\driving_school_management_system\\dbSystemDSMS.mdf\";Integrated Security=True");
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            // Check if the text box has a value
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Please enter a search term.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            string searchText = textBox1.Text.Trim();
+
+        
+
+            
+                // Open the connection
+            connection.Open();
+
+             
+                
+            SqlCommand command = new SqlCommand("SELECT * FROM Learner WHERE Id = @SearchText", connection);
+               
+            command.Parameters.AddWithValue("@SearchText", searchText);
+
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                if (reader.Read()) // Check if a row is found
+                {
+                    // Load details into textboxes
+                    textBox2.Text = reader["Name"].ToString();
+                    textBox3.Text = reader["Address"].ToString();
+                    textBox4.Text = reader["Contact No"].ToString();
+                    textBox5.Text = reader["License Type"].ToString();                
+                    connection.Close();
+
+                }
+                else
+                {
+                    // If no matching record is found, show a message
+                    MessageBox.Show("No record found.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    connection.Close();
+
+                }
+
+            }
+        }
+
+        private void calculateBtn_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox6.Text) && !string.IsNullOrWhiteSpace(textBox7.Text))
+            {
+                var total = double.Parse(textBox6.Text)-double.Parse(textBox6.Text)*double.Parse(textBox7.Text)/100;
+                textBox8.Text = total.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Enter Training Cost and Discount");
+            }
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Font font = new Font("Arial", 12);
+            Brush brush = Brushes.Black;
+
+            // Example: Print textbox values with labels
+            string nameLabelText = label1.Text;
+            string nameValueText = textBox1.Text; // Assuming textBoxName is the textbox for name
+            string formattedText = $"{nameLabelText} {nameValueText}";
+            g.DrawString(formattedText, font, brush, 100, 100);
+
+        }
+
+        private void printBtn_Click(object sender, EventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            printDialog.Document = printDocument1;
+
+            if (printDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Start printing
+                printDocument1.Print();
+            }
         }
     }
 }
